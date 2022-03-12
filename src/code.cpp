b9df888eed30a8c588654e64ce03e2e4bf2f66cf
2po4ill <90243310+2po4ill@users.noreply.github.com>
2022-03-12 14:13:41 +0300

    Update code.cpp

--- src/code.cpp
+++ src/code.cpp
@@ -5,72 +5,166 @@
 
 namespace assignment {
 
-  // Task 1
-  int squared_diff(int left, int right) {
+    // Task 1
+    int squared_diff(int left, int right) {
+        int a = left - right;
+        return a * a;
+    }
 
-    // Write your code here ...
+    // Task 2
+    bool check_bit(int mask, int bit_pos) {
+        if (mask >= 0 && bit_pos >= 0) {
+            int mask1 = mask;
+            int k = 0;
+            while (mask1) {
+                mask1 = mask1 / 2;
+                k++;
+            }
+            int A[k], i = 0;
+            while (mask) {
+                A[i] = mask % 2;
+                mask = mask / 2;
+                i++;
+            }
+            if (k > bit_pos && A[bit_pos] == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 
-    return 0;
-  }
+    // Task 3
+    int max3(int left, int middle, int right) {
+        int maxim = middle;
+        if (right > middle) {
+            maxim = right;
+        }
+        if (left > maxim) {
+            maxim = left;
+        }
+        return maxim;
+    }
 
-  // Task 2
-  bool check_bit(int mask, int bit_pos) {
+    // Task 4
+    void swap_args(int *left, int *right) {
+        if (left != nullptr && right != nullptr) {
+            int ukazleft = *left;
+            int ukazright = *right;
+            *right = ukazleft;
+            *left = ukazright;
+        }
+    }
 
-    // Write your code here ...
+    // Task 5
+    int arr_sum(int *arr, int length)
+    {
+        if (arr != nullptr)
+        {
+            if (length > 0)
+            {
+                int a = 0;
+                for (int i = 0; i < length; i++)
+                {
+                    a += arr[i];
+                }
+                return a;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else
+        {
+            return 0;
+        }
+    }
 
-    return false;
-  }
+    // Task 6
+    int *find_max_elem(int *arr, int length)
+    {
+        if (arr != nullptr && length > 0)
+        {
+            int max = arr[0];
+            int maxptr = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if(max<=arr[i])
+                {
+                    maxptr = i;
+                }
+            }
+            return arr + maxptr;
+        }
+        else
+        {
+            return nullptr;
+        }
+    }
 
-  // Task 3
-  int max3(int left, int middle, int right) {
+    // Task 7
+    int *allocate_arr(int length, int init_value)
+    {
+        if (length > 0) {
+            int arr[length];
+            for (int i = 0; i < length; i++)
+            {
+                arr[i] = init_value;
+            }
+            return arr;
+        }
+        else
+        {
+            return nullptr;
+        }
+    }
 
-    // Write your code here ...
+    // Task 8
+    int *clone_arr(int *arr_in, int length)
+    {
+        if (arr_in != nullptr && length > 0)
+        {
+            int arr[length];
+            for (int i = 0; i < length; i++)
+            {
+                arr[i] = arr_in[i];
+            }
+            return arr;
+        }
+        else
+        {
+            return nullptr;
+        }
+    }
 
-    return 0;
-  }
 
-  // Task 4
-  void swap_args(int *left, int *right) {
-
-    // Write your code here ...
-  }
-
-  // Task 5
-  int arr_sum(int *arr, int length) {
-
-    // Write your code here ...
-
-    return 0;
-  }
-
-  // Task 6
-  int *find_max_elem(int *arr, int length) {
-
-    // Write your code here ...
-
-    return nullptr;
-  }
-
-  // Task 7
-  int *allocate_arr(int length, int init_value) {
-
-    // Write your code here ...
-
-    return nullptr;
-  }
-
-  // Task 8
-  int *clone_arr(int *arr_in, int length) {
-
-    // Write your code here ...
-
-    return nullptr;
-  }
-
-  // Task 9
-  void print_kth_elements(int *arr, int length, int k, std::ostream &os) {
-
-    // Write your code here ...
-  }
-
-}  // namespace assignment
+    // Task 9
+    void print_kth_elements(int *arr, int length, int k, std::ostream &os)
+    {
+        if(arr!=nullptr)
+        {
+            if(length > 0)
+            {
+                if(k > 0)
+                {
+                    std::cout<<"1\t"<<arr[k]<<"\t"<<std::endl;
+                }
+                else
+                {
+                    std::cout<<"Invalid argument: k\n"<<std::endl;
+                }
+            }
+            else
+            {
+                std::cout<<"Invalid argument: length\n"<<std::endl;
+            }
+        }
+        else
+        {
+            std::cout<<"Invalid argument: arr\n"<<std::endl;
+        }
+    }  // namespace assignment
+}
